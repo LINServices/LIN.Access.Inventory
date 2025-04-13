f4f1658a05638eba4fb32df40c4ce36a7b0c06c3
Alex Giraldo <giraldojhong4@gmail.com>
2025-04-13 15:41:11 -0500

    Mejoras

---
+++ Controllers/Holds.cs
@@ -0,0 +1,51 @@
+﻿namespace LIN.Access.Inventory.Controllers;
+
+public static class Holds
+{
+
+    /// <summary>
+    /// Obtener todas las reservas asociadas a un inventario.
+    /// </summary>
+    /// <param name="inventory">Id del inventario.</param>
+    /// <param name="token">Token de acceso.</param>
+    public static async Task<ReadAllResponse<HoldModel>> ReadAll(int inventory, string token)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("holds/read/all");
+
+        // Headers.
+        client.AddHeader("id", inventory);
+        client.AddHeader("token", token);
+
+        // Resultado.
+        var Content = await client.Post<ReadAllResponse<HoldModel>>();
+
+        // Retornar.
+        return Content;
+    }
+
+
+    /// <summary>
+    /// Realizar la devolución de una reserva.
+    /// </summary>
+    /// <param name="holdGroup">Id del grupo de reserva.</param>
+    /// <param name="token">Token de acceso.</param>
+    public static async Task<ResponseBase> Return(int holdGroup, string token)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("holds");
+
+        // Headers.
+        client.AddHeader("id", holdGroup);
+        client.AddHeader("token", token);
+
+        // Resultado.
+        var Content = await client.Delete<ResponseBase>();
+
+        // Retornar.
+        return Content;
+    }
+
+}
---
+++ Controllers/OpenStore.cs
@@ -0,0 +1,109 @@
+﻿namespace LIN.Access.Inventory.Controllers;
+
+public static class OpenStore
+{
+
+
+    public static async Task<CreateResponse> CreateSettings(string token, string mercado, int inventory, string user, string password)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("OpenStoreSettings");
+
+        // Headers.
+        client.AddHeader("token", token);
+        client.AddHeader("mercadoToken", mercado);
+
+
+        client.AddParameter("user", user);
+        client.AddParameter("password", password);
+        client.AddParameter("inventory", inventory);
+
+        // Resultado.
+        var Content = await client.Post<CreateResponse>();
+
+        // Retornar.
+        return Content;
+
+
+    }
+
+
+    public static async Task<ReadOneResponse<OpenStoreSettings>> ReadSettings(string token, int inventory)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("OpenStoreSettings");
+
+        // Headers.
+        client.AddHeader("token", token);
+
+        client.AddParameter("inventory", inventory);
+
+        // Resultado.
+        var Content = await client.Get<ReadOneResponse<OpenStoreSettings>>();
+
+        // Retornar.
+        return Content;
+
+
+    }
+
+
+
+
+
+
+    public static async Task<ReadAllResponse<Types.Payments.Models.PayModel>> Payments(string token, int inventory)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("OpenStoreSettings/payments");
+
+        // Headers.
+        client.AddHeader("token", token);
+
+        client.AddParameter("inventory", inventory);
+
+        // Resultado.
+        var Content = await client.Get<ReadAllResponse<Types.Payments.Models.PayModel>>();
+
+        // Retornar.
+        return Content;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    /// <summary>
+    /// Crear salida online
+    /// </summary>
+    /// <param name="modelo">Modelo.</param>
+    /// <param name="token">Token de acceso.</param>
+    public static async Task<CreateResponse> CreateOnline(OutflowDataModel modelo, string token)
+    {
+
+        // Cliente HTTP.
+        Client client = Service.GetClient("connectors/OpenStore/hold");
+
+        // Headers.
+        client.AddHeader("token", token);
+
+        // Resultado.
+        var Content = await client.Post<CreateResponse>(modelo);
+
+        // Retornar.
+        return Content;
+
+
+    }
+}
